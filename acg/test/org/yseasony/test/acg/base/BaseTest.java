ace8efc03d1641d3dc07e5f08ae3aca0a9c25c10
yseasony <yseasony@63ded714-ee35-11de-9e3d-99312c638492>
2011-02-24 02:28:19 +0000

    test

---
+++ acg/test/org/yseasony/test/acg/base/BaseTest.java
@@ -0,0 +1,32 @@
+package org.yseasony.test.acg.base;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+/**
+ * 
+ * @author yseasony(yseasony@gmail.com)
+ */
+@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
+@TransactionConfiguration(defaultRollback = false,transactionManager="transactionManager")
+public class BaseTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+	protected MockHttpServletRequest request = new MockHttpServletRequest();
+	
+	protected MockHttpServletResponse response = new MockHttpServletResponse();
+	
+	protected MockHttpSession session = (MockHttpSession) request.getSession();
+
+	@Resource(name = "dataSource")
+	public void setDataSource(DataSource dataSource) {
+		super.setDataSource(dataSource);
+	}
+
+}
